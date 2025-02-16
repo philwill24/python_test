79f26633b084979c2e300869910d1d23e6f9c838
philwill117 <philwill117@gmail.com>
2025-02-16 08:42:51 -0600

    Fourth Commit

---
+++ test_calc.py
@@ -0,0 +1,13 @@
+def test_sum():
+    sum = 2 + 2
+    assert sum == 4
+
+ def test2_sum():
+    sum = 2 + 2
+    assert sum == 5
+
+def test_multiply():
+    a = 2
+    b = 3
+    multiply = a * b
+    assert multiply == 6
